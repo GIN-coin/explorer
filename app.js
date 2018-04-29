bdcd4d97240ba6588673217c4c7bc060a39112c9
GINcoin Dev <dev@gincoin.io>
2018-04-29 06:49:37 +0000

    GIN customization

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ if (settings.heavy != true) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, settings.favicon)));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
